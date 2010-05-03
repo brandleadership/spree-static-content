87a15c88ca9bd2c711c446684a4ab24bda0056a9
Roman Simecek <roman.simecek@screenconcept.ch>
2010-05-03 10:57:57 +0200

    model modified for sub pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ActiveRecord::Base
   default_scope :order => "position ASC"
 
   validates_presence_of :title
-  validates_presence_of [:slug, :body], :if => :not_using_foreign_link?
+  validates_presence_of :body, :if => :not_using_foreign_link?
   
   named_scope :header_links, :conditions => ["show_in_header = ?", true], :order => 'position'
   named_scope :footer_links, :conditions => ["show_in_footer = ?", true], :order => 'position'
@@ -25,25 +25,40 @@ class Page < ActiveRecord::Base
         Page.update_all("position = position - 1", ["? < position AND position <= ?", prev_position,  self.position])
       end
     end
-    
-    self.slug = slug_link
-    
+
+    self.slug = ( parent_page_id != 0 ? Page.find(parent_page_id).slug + slug_link : slug_link )
+
   end
 
   def link
-    foreign_link.blank? ? slug_link : foreign_link
+    if self.parent_page_id.eql?(0)
+      foreign_link.blank? ? slug_link : foreign_link
+    else
+      foreign_link.blank? ? Page.find(self.parent_page_id).link + slug_link : foreign_link
+    end
   end
 
-private  
+private
   def not_using_foreign_link?
     foreign_link.blank?
   end
 
   def slug_link
-    ensure_slash_prefix slug
+    ensure_slash_prefix normalize(title)
   end
   
   def ensure_slash_prefix(str)
     str.index('/') == 0 ? str : '/' + str
+  end
+
+  def normalize(slug_text)
+    return '' if slug_text.nil? || slug_text == ''
+    slug_text.
+      gsub(/[\W]/u, ' ').
+      strip.
+      gsub(/\s+/u, '-').
+      gsub(/-\z/u, '').
+      downcase.
+      to_s
   end
 end
