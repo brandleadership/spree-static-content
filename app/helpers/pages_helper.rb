fc07ee6904eb916bfa1fe26d2715c57f89e29efa
Roman Simecek <roman.simecek@screenconcept.ch>
2010-06-08 13:33:00 +0200

    if current page function implemented

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -11,5 +11,9 @@ module PagesHelper
   def static_content_root_page
     Page.find_by_root_page(true)
   end
-  
+
+  def static_content_current_page?(page)
+    page.slug.eql?request.env["PATH_INFO"]
+  end
+
 end
