40596c95980bd038854b8e86238cf4144a35b7e3
Roman Simecek <roman.simecek@screenconcept.ch>
2010-06-08 14:00:40 +0200

    removed code duplication and used named scope of the class

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,11 +1,11 @@
 module PagesHelper
 
   def static_content_sidebar
-    Page.find(:all, :conditions => {:show_in_sidebar => 1 , :visible => 1}, :order => :position)
+    Page.sidebar_links
   end
 
   def static_content_help_navigation
-    Page.find(:all, :conditions => {:show_in_help_navigation => 1 , :visible => 1}, :order => :position)
+    Page.help_links
   end
 
   def static_content_root_page
