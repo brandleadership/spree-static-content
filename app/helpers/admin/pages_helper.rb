7787355a6a174697240128c60ef9ac0a5f4407d6
Roman Simecek <roman.simecek@screenconcept.ch>
2010-06-10 11:32:46 +0200

    normalize locale added

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -3,5 +3,15 @@ module Admin::PagesHelper
   def sorted_pages
     Page.find(:all, :order => :slug)
   end
+
+  #
+  # Normalize the locale from spree-i18n to rails-i18n
+  # example: en-US => en_US
+  #
+  def normalize_locale(locale)
+    locale = locale.to_s
+
+    locale.tr('-','_')
+  end
   
 end
