5a501bedeee993e40c982d94eb45cc1e114460be
Roman Simecek <roman.simecek@screenconcept.ch>
2010-06-14 12:38:52 +0200

    code refactoring & correction

--- db/migrate/20100614073138_create_localization.rb
+++ db/migrate/20100614073138_create_localization.rb
@@ -1,15 +1,15 @@
 class CreateLocalization < ActiveRecord::Migration
   def self.up
-    rename_column(:pages, :body, "body_#{I18n.default_locale.to_s}".to_sym)
+    rename_column :pages, :body, "body_#{I18n.default_locale.to_s}".to_sym
     AVAILABLE_LOCALES.each do |lang|
-      add_column(:pages, "body_#{lang.first.sub('-','_')}".to_sym, :text) unless lang.first.sub('-','_') == I18n.default_locale.to_s
+      add_column :pages, "body_#{lang.first.sub('-','_')}".to_sym, :text unless lang.first.sub('-','_') == I18n.default_locale.to_s
     end
   end
 
   def self.down
-    rename_column(:pages, "body_#{I18n.default_locale.to_s}".to_sym, :body)
+    rename_column :pages, "body_#{I18n.default_locale.to_s}".to_sym, :body
     AVAILABLE_LOCALES.each do |lang|
-      remove_column(:pages, "body_#{lang.first.sub('-','_')}".to_sym, :text) unless lang.first.sub('-','_') == I18n.default_locale.to_s  
+      remove_column :pages, "body_#{lang.first.sub('-','_')}".to_sym unless lang.first.sub('-','_') == I18n.default_locale.to_s
     end
   end
 end
