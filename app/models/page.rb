4851c9187da289f02b963dc18df262a81504fe4f
Roman Simecek <roman.simecek@screenconcept.ch>
2010-06-17 14:54:08 +0200

    normalization extended with AcitveSupport::Multibyte and diacritics are now striped

--- app/models/page.rb
+++ app/models/page.rb
@@ -100,16 +100,61 @@ class Page < ActiveRecord::Base
     str.index('/') == 0 ? str : '/' + str
   end
 
+  private
+
+  #
+  # Sanitizes and dasherizes string to make it safe for URL's.
+  #
+  # Example:
+  #
+  #   slug.normalize('This... is an example!') # => "this-is-an-example"
+  #
+  # Note that the Unicode handling in ActiveSupport may fail to process some
+  # characters from Polish, Icelandic and other languages.
+  #
   def normalize(slug_text)
     return '' if slug_text.nil? || slug_text == ''
-
-    slug_text.
+    ActiveSupport::Multibyte.proxy_class.new(strip_diacritics(slug_text.to_s)).normalize(:kc).
       gsub(/[\W]/u, ' ').
       strip.
       gsub(/\s+/u, '-').
       gsub(/-\z/u, '').
       downcase.
       to_s
+  end
+
+  #
+  # Slug generation ascii approximations
+  #
+  ASCII_APPROXIMATIONS = {
+    198 => "AE",
+    208 => "D",
+    216 => "O",
+    222 => "Th",
+    223 => "ss",
+    230 => "ae",
+    240 => "d",
+    248 => "o",
+    254 => "th",
+    8217 => "", # apostrophe Õ
+    187 => "",  # guillemets È
+    171 => ""   # guillemets Ç
+  }.freeze
+
+  #
+  # Remove diacritics (accents, umlauts, etc.) from the string. Borrowed
+  # from "The Ruby Way."
+  #
+  def strip_diacritics(string)
+    a = ActiveSupport::Multibyte.proxy_class.new(string || "").normalize(:kd)
+    a.unpack('U*').inject([]) { |a, u|
+      if ASCII_APPROXIMATIONS[u]
+        a += ASCII_APPROXIMATIONS[u].unpack('U*')
+      elsif (u < 0x300 || u > 0x036F)
+        a << u
+      end
+      a
+    }.pack('U*')
   end
 
 end
